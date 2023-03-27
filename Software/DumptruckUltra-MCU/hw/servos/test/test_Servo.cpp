4bd6ff62462bf24b28e351bfc407ccb381dcf410
Ben Nowotny <bennowotny65535@gmail.com>
2023-03-27 11:39:53 -0500

    Adding clangd help

---
+++ Software/DumptruckUltra-MCU/hw/servos/test/test_Servo.cpp
@@ -0,0 +1,32 @@
+#include "cyhal_system.h"
+#include "servos/Servo.hpp"
+
+namespace Hardware {
+namespace Servos {
+namespace test {
+void runTest() {
+    // Setup
+    using Hardware::Servos::Servo;
+    Servo testServo{P9_3};
+    testServo.enable();
+
+    float position{0.0};
+    bool goingUp{true};
+    constexpr float STEP{100.0 / 10.0 / 100.0};
+
+    // Climb the available duty cycles
+    // FOR TESTER: Check w/ scope that the lowest high time is 600us and the highest high tim is 2400 us
+    while (true) {
+        testServo.setPosition(position);
+        cyhal_system_delay_ms(10);
+        if (goingUp && position < 99)
+            position += STEP;
+        else if (!goingUp && position > 1)
+            position -= STEP;
+        else
+            goingUp = !goingUp;
+    }
+}
+} // namespace test
+} // namespace Servos
+} // namespace Hardware
---
+++ Software/DumptruckUltra-MCU/hw/servos/test/test_Servo.hpp
@@ -0,0 +1,12 @@
+#ifndef HW_SERVO_TEST_TEST_SERVO_HPP_
+#define HW_SERVO_TEST_TEST_SERVO_HPP_
+
+namespace Hardware {
+namespace Servos {
+namespace test {
+void runTest();
+}
+} // namespace Servos
+} // namespace Hardware
+
+#endif
