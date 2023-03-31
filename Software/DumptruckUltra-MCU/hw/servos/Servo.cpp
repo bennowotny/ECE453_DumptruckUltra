f56dfbb3547b998eb09c118524585ff790958f59
Ben Nowotny <bennowotny65535@gmail.com>
2023-03-31 13:29:19 -0500

    Fixing typo

--- Software/DumptruckUltra-MCU/hw/servos/Servo.cpp
+++ Software/DumptruckUltra-MCU/hw/servos/Servo.cpp
@@ -12,7 +12,7 @@ namespace Hardware {
 namespace Servos {
 
 Servo::Servo(cyhal_gpio_pin pin) : pwmHandle{} {
-    // Ivert so that we can use the hardware inversion to produce a normal signal
+    // Invert so that we can use the hardware inversion to produce a normal signal
     auto result{cyhal_pwm_init_adv(&pwmHandle, pin, NC, CYHAL_PWM_RIGHT_ALIGN, true, 0U, true, nullptr)};
     CY_ASSERT(CY_RSLT_SUCCESS == result);
 }
