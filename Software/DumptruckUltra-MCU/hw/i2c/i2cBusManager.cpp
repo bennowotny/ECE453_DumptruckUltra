b19724603391dcc7d5b247e8aa0eac5d9390cc82
srao32 <55770703+srao32@users.noreply.github.com>
2023-04-03 14:10:20 -0500

    Update Software/DumptruckUltra-MCU/hw/i2c/i2cBusManager.cpp
    Co-authored-by: Ben Nowotny <44074469+bennowotny@users.noreply.github.com>

--- Software/DumptruckUltra-MCU/hw/i2c/i2cBusManager.cpp
+++ Software/DumptruckUltra-MCU/hw/i2c/i2cBusManager.cpp
@@ -45,8 +45,7 @@ cy_rslt_t I2CBusManager::i2cReadReg(uint16_t devAddr, uint8_t reg, uint8_t *data
 
 	cy_rslt_t rslt = cyhal_i2c_master_write(&i2cMonarchObj, devAddr, &reg, 1, 100, false);
 
-	if(rslt != CY_RSLT_SUCCESS)
-				 while(1);
+	CY_ASSERT(rslt == CY_RSLT_SUCCESS);
 
 	rslt = cyhal_i2c_master_read(&i2cMonarchObj, devAddr, data, size, 100, true);
 
