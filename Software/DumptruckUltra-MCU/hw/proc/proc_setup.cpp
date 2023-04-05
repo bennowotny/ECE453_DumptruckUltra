2c90688ac08a0fd369d4dd7b9681c6aa2e06a9c6
Ben Nowotny <bennowotny65535@gmail.com>
2023-04-05 14:55:13 -0500

    Creating a shared blinky task

--- Software/DumptruckUltra-MCU/hw/proc/proc_setup.cpp
+++ Software/DumptruckUltra-MCU/hw/proc/proc_setup.cpp
@@ -11,5 +11,31 @@ void setupProcessor() {
 
     __enable_irq();
 }
+
+FreeRTOSBlinky::FreeRTOSBlinky() {
+    const auto drivingTaskSetupResult{
+        xTaskCreate(
+            [](void *obj) { FreeRTOSBlinky::ledTask(); },
+            BLINKY_TASK_NAME,
+            BLINKY_STACK_SIZE,
+            nullptr,
+            BLINKY_PRIORITY,
+            nullptr)};
+    CY_ASSERT(drivingTaskSetupResult == pdPASS);
+}
+
+void FreeRTOSBlinky::ledTask() {
+
+    // Initialize LED
+    const auto res{cyhal_gpio_init(P5_5, CYHAL_GPIO_DIR_OUTPUT, CYHAL_GPIO_DRIVE_STRONG, false)};
+    CY_ASSERT(res == CY_RSLT_SUCCESS);
+
+    // Toggle LED
+    while (true) {
+        cyhal_gpio_toggle(P5_5);
+        vTaskDelay(pdMS_TO_TICKS(500));
+    }
+}
+
 } // namespace Processor
 } // namespace Hardware
