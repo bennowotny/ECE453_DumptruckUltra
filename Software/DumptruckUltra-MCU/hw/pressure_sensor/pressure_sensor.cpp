567af4a3ec327e6b287a2fae56a6602ca1b1131b
abishekkannan23 <abishek.kannan@yahoo.com>
2023-04-07 13:28:46 -0500

    added untested pressure sensor ADC code

---
+++ Software/DumptruckUltra-MCU/hw/pressure_sensor/pressure_sensor.cpp
@@ -0,0 +1,35 @@
+#include "pressure_sensor.hpp"
+#include "cy_result.h"
+#include "cyhal_adc.h"
+#include "pressure_sensor/pressure_sensor.hpp"
+#include <iostream>
+
+namespace Hardware {
+namespace Pressure_Sensor {
+
+Pressure::Pressure(cyhal_gpio_t pin) : adcHandle{},
+                                       adc_chan_0_obj{} {
+    // Initialize ADC
+    auto rslt{cyhal_adc_init(&adcHandle, pin, nullptr)};
+    CY_ASSERT(CY_RSLT_SUCCESS == rslt);
+
+    // Initialize ADC channel, allocate channel 0 to "pin"
+    cyhal_adc_channel_config_t channel_config = {.enabled = true, .enable_averaging = false, .min_acquisition_ns = 220};
+    auto result{cyhal_adc_channel_init_diff(&adc_chan_0_obj, &adcHandle, pin, CYHAL_ADC_VNEG, &channel_config)};
+    CY_ASSERT(CY_RSLT_SUCCESS == result);
+}
+
+void Pressure::read() {
+    // Read ADC conversion result for corresponding ADC channel
+    auto adc_out{cyhal_adc_read(&adc_chan_0_obj)};
+    std::cout << adc_out;
+}
+
+void Pressure::free() {
+    // Release ADC and channel objects when no longer needed
+    cyhal_adc_channel_free(&adc_chan_0_obj);
+    cyhal_adc_free(&adcHandle);
+}
+
+} // namespace Pressure_Sensor
+} // namespace Hardware
---
+++ Software/DumptruckUltra-MCU/hw/pressure_sensor/pressure_sensor.hpp
@@ -0,0 +1,27 @@
+
+
+#ifndef HW_PRESSURE_SENSOR_HPP_
+#define HW_PRESSURE_SENSOR_HPP_
+
+#include "cyhal_adc.h"
+#include "cyhal_hw_types.h"
+#include <cstdint>
+
+namespace Hardware {
+namespace Pressure_Sensor {
+
+class Pressure {
+public:
+    explicit Pressure(cyhal_gpio_t pin);
+    void read();
+    void free();
+
+private:
+    cyhal_adc_t adcHandle;
+    cyhal_adc_channel_t adc_chan_0_obj;
+};
+
+} // namespace Pressure_Sensor
+} // namespace Hardware
+
+#endif
