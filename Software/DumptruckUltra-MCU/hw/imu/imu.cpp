1274db17d8c39da46596005a31567256f7e1bd60
srao32 <srao32@wisc.edu>
2023-04-07 16:22:53 -0500

    Added function definitions to header

--- Software/DumptruckUltra-MCU/hw/imu/imu.cpp
+++ Software/DumptruckUltra-MCU/hw/imu/imu.cpp
@@ -1,8 +1,11 @@
 #include "hw/imu/imu.hpp"
+#include "FreeRTOS.h"
 #include "cyhal_psoc6_01_43_smt.h"
-#include "i2c/i2cBusManager.hpp"
+#include "i2cBusManager.hpp"
 
-IMU(cyhal_gpio_t sda, cyhal_gpio_t scl) : i2cBus(sda, scl) {
+IMU::IMU(const Hardware::I2C::I2CBusManager::i2cPin_t &i2cPins)
+    : i2cBus(i2cPins) {
     // Write CTRL1_XL AND CTRL2_G to set Output Data Rate (ODR)
     // Write CTRL6_C?
+    pdMS_TO_TICKS(5);
 }
--- Software/DumptruckUltra-MCU/hw/imu/imu.hpp
+++ Software/DumptruckUltra-MCU/hw/imu/imu.hpp
@@ -1,14 +1,29 @@
 #pragma once
 
-#include "cyhal_psoc6_01_43_smt.h"
-#include "i2c/i2cBusManager.hpp"
+#include "FreeRTOS.h"
+#include "hw/i2c/i2cBusManager.hpp"
+#include "portmacro.h"
+#include "queue.h"
+#include <array>
+#include <cstddef>
 
 class IMU {
 public:
-    IMU();
+    struct imuData_t {
+        std::array<float, 3> accel_float; // X, Y, Z
+        std::array<float, 3> gyro_float;  // X, Y, Z
+        uint32_t timestamp;
+    };
+
+    IMU(const Hardware::I2C::I2CBusManager::i2cPin_t &);
+
+    template <std::size_t N>
+    auto getIMUData(std::array<imuData_t, N> dataBuf, TickType_t tickToWait) -> void;
 
 private:
     static constexpr uint8_t IMU_ADDR{0x6A};
+
     Hardware::I2C::I2CBusManager i2cBus;
     // Queue of some sort. FreeRTOS Queue?
-}
+    QueueHandle_t imuQueue;
+};
