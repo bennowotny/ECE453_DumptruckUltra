5c24ddc43ef5d73524e0644060dae4c923789b82
srao32 <srao32@wisc.edu>
2023-04-12 00:09:19 -0500

    Added constants for register numbers and values

--- Software/DumptruckUltra-MCU/hw/imu/imu.cpp
+++ Software/DumptruckUltra-MCU/hw/imu/imu.cpp
@@ -6,13 +6,26 @@
 
 namespace Hardware {
 namespace IMU {
+// https://github.com/stm32duino/LSM6DSOX
 IMU::IMU(std::shared_ptr<Hardware::I2C::I2CBusManager> i2cBus) : i2cBus{std::move(i2cBus)} {
     // Write CTRL1_XL AND CTRL2_G to set Output Data Rate (ODR)
     // Write CTRL6_C?
 
     // Set up interrupt pin
 
-    // Initialize sensors
+    // Disable I3C
+
+    // Register address auto-incremented on multi-byte read by default
+
+    // Enable block data update
+
+    // Select FIFO Mode
+
+    // Set BDR counter threshold
+
+    // Configure interrupt generation on IMU
+
+    // Enable sensors
 
     // Check device ID
 }
--- Software/DumptruckUltra-MCU/hw/imu/imu.hpp
+++ Software/DumptruckUltra-MCU/hw/imu/imu.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "FreeRTOS.h"
+#include "cy_syslib.h"
 #include "hw/i2c/i2cBusManager.hpp"
 #include "portmacro.h"
 #include "queue.h"
@@ -30,8 +31,27 @@ public:
     explicit IMU(std::shared_ptr<Hardware::I2C::I2CBusManager>);
 
 private:
-    static constexpr uint8_t IMU_ADDR{0x6A};
     const std::shared_ptr<Hardware::I2C::I2CBusManager> i2cBus;
+
+    static constexpr uint8_t IMU_ADDR{0x6A};
+
+    static constexpr uint8_t CTRL1_XL{0x10};
+    static constexpr uint8_t CTRL2_G{0x11};
+    static constexpr uint8_t CTRL9_XL{0x18};
+    static constexpr uint8_t CTRL3_C{0x12};
+    static constexpr uint8_t FIFO_CTRL4{0x0A};
+    static constexpr uint8_t COUNTER_BDR_REG1{0x0B};
+    static constexpr uint8_t COUNTER_BDR_REG2{0x0C};
+    static constexpr uint8_t INT1_CTRL{0x0D};
+
+    static constexpr uint8_t XL_CTRL{0x40};
+    static constexpr uint8_t G_CTRL{0x4C};
+    static constexpr uint8_t XL_DISABLE_I3C{0xE2};
+    static constexpr uint8_t SET_BLK_DATA_UPDATE{0x44};
+    static constexpr uint8_t FIFO_MODE{0x41};
+    static constexpr uint8_t CONT_MODE{0x46};
+    static constexpr uint8_t CNT_BDR_TH{10};
+    static constexpr uint8_t EN_BDR_INT{0x40};
 };
 } // namespace IMU
 } // namespace Hardware
