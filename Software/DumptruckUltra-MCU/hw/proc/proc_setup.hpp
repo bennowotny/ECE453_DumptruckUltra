1249876ced4c8f893aee74f96e8ccc85727a5ee0
Ben Nowotny <bennowotny65535@gmail.com>
2023-04-19 13:34:58 -0500

    Re-defined pins for consistent naming

--- Software/DumptruckUltra-MCU/hw/proc/proc_setup.hpp
+++ Software/DumptruckUltra-MCU/hw/proc/proc_setup.hpp
@@ -11,39 +11,42 @@
 namespace Hardware {
 
 namespace Processor {
-constexpr cyhal_gpio_t MCU_RX_PI_TX(P5_0);
-constexpr cyhal_gpio_t MCU_TX_PI_RX(P5_1);
+constexpr cyhal_gpio_t MCU_RX_PI_TX{P5_0};
+constexpr cyhal_gpio_t MCU_TX_PI_RX{P5_1};
 
-constexpr cyhal_gpio_t MI_FORWARD(P5_2);
-constexpr cyhal_gpio_t M1_Backward(P5_4);
-constexpr cyhal_gpio_t M2_Forward(P5_6);
-constexpr cyhal_gpio_t M2_Backward(P6_2);
+constexpr cyhal_gpio_t MI_FORWARD{P5_2};
+constexpr cyhal_gpio_t M1_BACKWARD{P5_4};
+constexpr cyhal_gpio_t M2_FORWARD{P5_6};
+constexpr cyhal_gpio_t M2_BACKWARD{P6_2};
 
-constexpr cyhal_gpio_t Servo1_PWM(P5_3);
-constexpr cyhal_gpio_t Servo2_PWM(P5_5);
-constexpr cyhal_gpio_t Servo3_PWM(P6_3);
-constexpr cyhal_gpio_t Servo4_PWM(P10_1);
-constexpr cyhal_gpio_t Servo5_PWM(P10_3);
-constexpr cyhal_gpio_t Servo6_PWM(P10_5);
-constexpr cyhal_gpio_t Servo7_PWM(P12_7);
+constexpr cyhal_gpio_t SERVO1_PWM{P5_3};
+constexpr cyhal_gpio_t SERVO2_PWM{P5_5};
+constexpr cyhal_gpio_t SERVO3_PWM{P6_3};
+constexpr cyhal_gpio_t SERVO4_PWM{P10_1};
+constexpr cyhal_gpio_t SERVO5_PWM{P10_3};
+constexpr cyhal_gpio_t SERVO6_PWM{P10_5};
+constexpr cyhal_gpio_t SERVO7_PWM{P12_7};
 
-constexpr cyhal_gpio_t MCU_RX_FTDI_TX(P6_4);
-constexpr cyhal_gpio_t MCU_TX_FTDI_RX(P6_5);
+constexpr cyhal_gpio_t MCU_RX_FTDI_TX{P6_4};
+constexpr cyhal_gpio_t MCU_TX_FTDI_RX{P6_5};
 
 // SWDIO already defined
-constexpr cyhal_gpio_t SWDCLK(P6_7);
+constexpr cyhal_gpio_t SWDCLK{P6_7};
 
-constexpr cyhal_gpio_t USER_LED(P7_1);
-constexpr cyhal_gpio_t PB_IN(P7_2);
+constexpr cyhal_gpio_t USER_LED{P7_1};
+constexpr cyhal_gpio_t USER_RGB_RED{P9_4};
+constexpr cyhal_gpio_t USER_RGB_GREEN{P9_5};
+constexpr cyhal_gpio_t USER_RGB_BLUE{P9_6};
+constexpr cyhal_gpio_t PB_IN{P7_2};
 
-constexpr cyhal_gpio_t I2C_SCL(P9_0);
-constexpr cyhal_gpio_t I2C_SDA(P9_1);
+constexpr cyhal_gpio_t I2C_SCL{P9_0};
+constexpr cyhal_gpio_t I2C_SDA{P9_1};
 
-constexpr cyhal_gpio_t IMU_INT(P9_2);
+constexpr cyhal_gpio_t IMU_INT{P9_2};
 
-constexpr cyhal_gpio_t DIST_INT(P9_3);
+constexpr cyhal_gpio_t DIST_INT{P9_3};
 
-constexpr cyhal_gpio_t PressureSensorADC(P10_0);
+constexpr cyhal_gpio_t PRESSURE_SENSOR_ADC{P10_0};
 
 void setupProcessor();
 
