0c4d92d4a6c9cae4f50af8fd99a26879f31738ca
Ben Nowotny <bennowotny65535@gmail.com>
2023-04-28 13:57:48 -0500

    fixing build

--- Software/DumptruckUltra-MCU/logic/fsm/fsmStates.cpp
+++ Software/DumptruckUltra-MCU/logic/fsm/fsmStates.cpp
@@ -6,15 +6,15 @@
 #include "arm-inverse-kinematics/ArmControl.hpp"
 #include "fsm/DumptruckUltra.hpp"
 #include "pressure_sensor.hpp"
-#include "proc_setup.hpp"
 #include "projdefs.h"
 #include <memory>
+#include <random>
 
 namespace Logic {
 namespace FSM {
 
 auto initStateAction() -> DumptruckUltra::FSMState {
-    printf("%s", "Doing init state\n");
+    // printf("%s", "Doing init state\n");
     vTaskDelay(pdMS_TO_TICKS(500));
     return DumptruckUltra::FSMState::DRIVE_TO_SEARCH;
 }
@@ -22,22 +22,29 @@ auto initStateAction() -> DumptruckUltra::FSMState {
 // TODO: Take in driving algo as param
 // Make random number 1-10 for x and y coordinates
 // start() allows for repeated calls to getStatus()
-auto driveToSearchAction(const std::shared_ptr<Logic::DrivingAlgorithm::DrivingAlgorithm> &drivingAlg) -> DumptruckUltra::FSMState {
+auto driveToSearchAction(Logic::DrivingAlgorithm::DrivingAlgorithm &drivingAlg) -> DumptruckUltra::FSMState {
     // printf("%s", "Doing driveToSearch state\n");
-    float randX = 1 + rand() % 10;
-    float randY = 1 + rand() % 10;
-    printf("%d", drivingAlg->getStatus());
-    drivingAlg->loadNewTarget({.x = randX, .y = randY, .heading = 0});
-    printf("%d", drivingAlg->getStatus());
-    drivingAlg->start();
-    printf("%d", drivingAlg->getStatus());
-    vTaskDelay(pdMS_TO_TICKS(500));
+    std::random_device randomSource{};
+    std::default_random_engine randomEngine{randomSource()};
+    std::uniform_real_distribution<float> distribution{0, 10};
+    const float randX{distribution(randomEngine)};
+    const float randY{distribution(randomEngine)};
+
+    // printf("%d", drivingAlg->getStatus());
+    drivingAlg.loadNewTarget({.x = randX, .y = randY, .heading = 0});
+    // printf("%d", drivingAlg->getStatus());
+    drivingAlg.start();
+    while (drivingAlg.getStatus() != DrivingAlgorithm::DrivingAlgorithmStatus::COMPLETE) {
+        vTaskDelay(pdMS_TO_TICKS(5));
+    }
+
+    // printf("%d", drivingAlg->getStatus());
     return DumptruckUltra::FSMState::LOCAL_SEARCH;
 }
 
 auto localSearchAction() -> DumptruckUltra::FSMState {
-    printf("%s", "Doing localSearch state\n");
-    vTaskDelay(pdMS_TO_TICKS(500));
+    // printf("%s", "Doing localSearch state\n");
+
     return DumptruckUltra::FSMState::APPROACH;
 }
 
@@ -48,11 +55,11 @@ auto approachAction() -> DumptruckUltra::FSMState {
 }
 
 // TODO: Uses Camera Reading
-auto pickupAction(const std::shared_ptr<Hardware::DistanceSensor::DistanceSensor> &distSensor, std::unique_ptr<Logic::Arm::ArmControl> arm, const std::shared_ptr<Hardware::PressureSensor::PressureSensor> &pressureSensor) -> DumptruckUltra::FSMState {
+auto pickupAction(Hardware::DistanceSensor::DistanceSensor &distSensor, Logic::Arm::ArmControl &arm, Hardware::PressureSensor::PressureSensor &pressureSensor) -> DumptruckUltra::FSMState {
     // printf("%s", "Doing pickup state\n");
     float pickupDistance = 20.0;
     // Distance sensor reading and camera recognize object is at right distance
-    if (distSensor->getDistanceMeters() != pickupDistance) {
+    if (distSensor.getDistanceMeters() != pickupDistance) {
         return DumptruckUltra::FSMState::APPROACH;
     }
     return DumptruckUltra::FSMState::DRIVE_TO_START;
@@ -64,16 +71,16 @@ auto driveToStartAction() -> DumptruckUltra::FSMState {
     return DumptruckUltra::FSMState::DISPENSE;
 }
 
-auto dispenseAction(const std::shared_ptr<Hardware::Servos::Servo> &dispenserServo) -> DumptruckUltra::FSMState {
+auto dispenseAction(Hardware::Servos::Servo &dispenserServo) -> DumptruckUltra::FSMState {
     // printf("%s", "Doing dispense state\n");
     // TODO: test for dispenser servo value, assume 22 for now
     float dispenserOpen = 22.0;
     float dispenserClose = 0.0;
     // Open Dispenser
-    dispenserServo->setPosition(dispenserOpen);
+    dispenserServo.setPosition(dispenserOpen);
     vTaskDelay(pdMS_TO_TICKS(500));
     // Close Dispenser
-    dispenserServo->setPosition(dispenserClose);
+    dispenserServo.setPosition(dispenserClose);
     return DumptruckUltra::FSMState::DRIVE_TO_SEARCH;
 }
 
--- Software/DumptruckUltra-MCU/logic/fsm/fsmStates.hpp
+++ Software/DumptruckUltra-MCU/logic/fsm/fsmStates.hpp
@@ -1,17 +1,21 @@
 #pragma once
 
 #include "DumptruckUltra.hpp"
+#include "hw/distance_sensor/DistanceSensor.hpp"
+#include "hw/pressure_sensor/pressure_sensor.hpp"
+#include "hw/servos/Servo.hpp"
+#include "logic/arm-inverse-kinematics/ArmControl.hpp"
 
 namespace Logic {
 namespace FSM {
 
 auto initStateAction() -> DumptruckUltra::FSMState;
-auto driveToSearchAction() -> DumptruckUltra::FSMState;
+auto driveToSearchAction(Logic::DrivingAlgorithm::DrivingAlgorithm &drivingAlg) -> DumptruckUltra::FSMState;
 auto localSearchAction() -> DumptruckUltra::FSMState;
 auto approachAction() -> DumptruckUltra::FSMState;
-auto pickupAction() -> DumptruckUltra::FSMState;
+auto pickupAction(Hardware::DistanceSensor::DistanceSensor &distSensor, Logic::Arm::ArmControl &arm, Hardware::PressureSensor::PressureSensor &pressureSensor) -> DumptruckUltra::FSMState;
 auto driveToStartAction() -> DumptruckUltra::FSMState;
-auto dispenseAction() -> DumptruckUltra::FSMState;
+auto dispenseAction(Hardware::Servos::Servo &dispenserServo) -> DumptruckUltra::FSMState;
 
 } // namespace FSM
 } // namespace Logic
