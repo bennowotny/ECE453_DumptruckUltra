1800a5c65449978bceb8e489a9e827d6846c8104
srao32 <srao32@wisc.edu>
2023-05-01 12:18:12 -0500

    Updates to driving algorithm integration with IMU

--- Software/DumptruckUltra-MCU/logic/driving-algorithm/test/test_DrivingWithDeadReckoning.cpp
+++ Software/DumptruckUltra-MCU/logic/driving-algorithm/test/test_DrivingWithDeadReckoning.cpp
@@ -3,6 +3,7 @@
 #include "DrivingAlgorithm.hpp"
 #include "FreeRTOSConfig.h"
 #include "Motor.hpp"
+#include "Positioning.hpp"
 #include "cy_retarget_io.h"
 #include "cyhal_psoc6_01_43_smt.h"
 #include "i2cBusManager.hpp"
@@ -24,14 +25,14 @@ auto main() -> int {
     using Logic::DeadReckoning::DeadReckoning;
     Hardware::Processor::setupProcessor();
 
-    std::unique_ptr<Logic::DeadReckoning::DeadReckoning> deadReckoning{};
-
     Logic::DrivingAlgorithm::DriveMotorLayout layout{
         .leftMotor = Hardware::Motors::Motor{{.forwardPin = Hardware::Processor::M1_FORWARD, .backwardPin = Hardware::Processor::M1_BACKWARD}, Hardware::Motors::MotorDirection::FORWARD},
         .rightMotor = Hardware::Motors::Motor{{.forwardPin = Hardware::Processor::M2_FORWARD, .backwardPin = Hardware::Processor::M2_BACKWARD}, Hardware::Motors::MotorDirection::REVERSE}};
 
     Hardware::I2C::i2cPin_t i2cPins{.sda = Hardware::Processor::I2C_SDA, .scl = Hardware::Processor::I2C_SCL};
     auto i2cBus{std::make_shared<I2CBusManager>(i2cPins)};
+
+    auto deadReckoning{std::make_shared<DeadReckoning>()};
 
     auto uut =
         std::make_unique<Logic::DrivingAlgorithm::DrivingAlgorithm>(
@@ -41,13 +42,11 @@ auto main() -> int {
             // FOR TESTER: Change this line to simulate different current positions
             [deadReckoning]() -> auto{ return deadReckoning->getCurrentPose(); });
 
-    deadReckoning = std::make_shared<DeadReckoning>([uu2{uut.get()}]() { return uu2->getPower(); });
+    deadReckoning->setMotorSpeedsHandle([uu2{uut.get()}]() -> auto{ return uu2->getPower(); });
 
     auto imu{std::make_unique<IMU>(
         i2cBus,
-        [uu2{deadReckoning.get()}](const Hardware::IMU::AccelerometerData &msg) -> void { 
-            printf("%f\n", msg.Ax);
-            uu2->sendAccelerometerMessage(msg); },
+        [uu2{deadReckoning.get()}](const Hardware::IMU::AccelerometerData &msg) -> void { uu2->sendAccelerometerMessage(msg); },
         [uut2{deadReckoning.get()}](const Hardware::IMU::GyroscopeData &msg) -> void { uut2->sendGyroscopeMessage(msg); },
         printStr)};
 
@@ -63,9 +62,9 @@ auto main() -> int {
 
     xTaskCreate(
         [](void *params) -> void {
-            auto dr = static_cast<Logic::DrivingAlgorithm::DrivingAlgorithm *>(params);
+            auto da = static_cast<Logic::DrivingAlgorithm::DrivingAlgorithm *>(params);
             while (true) {
-                if (dr->getStatus() == Logic::DrivingAlgorithm::DrivingAlgorithmStatus::COMPLETE) {
+                if (da->getStatus() == Logic::DrivingAlgorithm::DrivingAlgorithmStatus::COMPLETE) {
                     printf("Driving algorithm finished!\r\n");
                     vTaskDelay(portMAX_DELAY);
                 }
@@ -75,6 +74,21 @@ auto main() -> int {
         "Poll Driving Algorithm",
         2 * configMINIMAL_STACK_SIZE,
         uut.get(),
+        tskIDLE_PRIORITY + 1,
+        nullptr);
+
+    xTaskCreate(
+        [](void *params) -> void {
+            auto dr = static_cast<Logic::DeadReckoning::DeadReckoning *>(params);
+            while (true) {
+                Logic::DeadReckoning::Pose2D currPos = dr->getCurrentPose();
+                printf("%f %f %f\n", currPos.x, currPos.y, currPos.heading);
+                vTaskDelay(pdMS_TO_TICKS(500));
+            }
+        },
+        "Poll DeadReckoning",
+        1 * configMINIMAL_STACK_SIZE,
+        deadReckoning.get(),
         tskIDLE_PRIORITY + 1,
         nullptr);
 
